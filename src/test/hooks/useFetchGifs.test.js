ee9f16677a4551170f4f651372518e2e179601e0
Alexis Guanique <guaniqued@gmail.com>
2022-05-26 19:20:17 -0300

    Commit Adicional

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -1,4 +1,4 @@
-;import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
 import { useFetchGifs } from "../../hooks/useFetchGifs"
 
 describe('Pruebas en el hook useFetchGifs', () => {
